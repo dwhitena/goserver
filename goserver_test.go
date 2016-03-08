245fac33b120171cbec27c2a6abb2cef5237f4a9
Daniel Whitenack <whitenack.daniel@gmail.com>
2016-03-08 17:00:07 -0600

    add loggin and router tests

--- goserver_test.go
+++ goserver_test.go
@@ -9,3 +9,24 @@ import (
 func TestStub(t *testing.T) {
 	assert.True(t, true, "This is good. Canary test passing")
 }
+
+// TestLogger is a test to make sure REST requests are logged
+func TestLogger(t *testing.T) {
+	for _, route := range routes {
+
+		// wrap all current routes in the logger decorator to log out requests
+		testhandler := Logger(route.HandlerFunc, route.Name)
+		if &testhandler == nil {
+			t.Error("Test failed: Could not wrap route with logger")
+		}
+
+	}
+}
+
+// TestNewRouter is a test to make sure a new router can be created
+func TestNewRouter(t *testing.T) {
+	testrouter := NewRouter()
+	if &testrouter == nil {
+		t.Error("Test failed: Could not create new router")
+	}
+}
